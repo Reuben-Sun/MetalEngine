5fb2245964bc27846d18c59dc1649f79ebe0ace0
sunzheng <2824016820@qq.com>
2022-06-30 01:43:30 +0800

    init

--- Engine.playground/Contents.swift
+++ Engine.playground/Contents.swift
@@ -1,3 +1,16 @@
-import Cocoa
+import PlaygroundSupport
+import MetalKit
 
-var greeting = "Hello, playground"
+//获取默认设备
+guard let device = MTLCreateSystemDefaultDevice() else{
+    fatalError("GPU is not support")
+}
+
+//初始化窗口尺寸、清屏颜色
+let frame = CGRect(x: 0, y: 0, width: 600, height: 600)
+let view = MTKView(frame: frame, device: device)
+view.clearColor = MTLClearColor(red: 1, green: 1, blue: 0.8, alpha: 1)
+
+//模型IO
+
+
